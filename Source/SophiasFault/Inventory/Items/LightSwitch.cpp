3b584333a8a2ad36731219c8325dd90788af2c41
Aleix-Soler-Manuel <aleixsolermanuel@gmail.com>
2024-05-11 02:59:34 +0200

    FEAT: Meshes and textures
    - Added all the bath decoration
    - Corrected the common roof mesh
    - Added living room meshes and textures

--- Source/SophiasFault/Inventory/Items/LightSwitch.cpp
+++ Source/SophiasFault/Inventory/Items/LightSwitch.cpp
@@ -1,25 +1,69 @@
 #include "LightSwitch.h"
 #include "Components/PointLightComponent.h" 
+#include "Curves/CurveFloat.h"
 
 ALightSwitch::ALightSwitch()
 {
+	_bLightOn = false;
+	_bOldLight = false;
+
+	_curveFloat = CreateDefaultSubobject<UCurveFloat>(TEXT("CurveFloat"));
+
+	_lightSwitch = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LightSwitch"));
+}
+
+void ALightSwitch::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Timeline
+	if (_curveFloat) {
+		_curveFloat->FloatCurve.UpdateOrAddKey(0.f, 0.f);
+
+		if (_bOldLight)
+			_curveFloat->FloatCurve.UpdateOrAddKey(1.f, 90.f);
+		else
+			_curveFloat->FloatCurve.UpdateOrAddKey(1.f, 45.f);
+
+		_timelineCallback.BindUFunction(this, FName("ControlLightSwitch"));
+
+		_timelineComponent->AddInterpFloat(_curveFloat, _timelineCallback);
+	}
 }
 
 void ALightSwitch::UseInteraction(AItem* item)
 {
 	IInteractiveInterface::UseInteraction(item);
 
-	for (auto& lights : _lightActor) {
-		if (lights) {
-			if (lights->GetClass()->GetName() == "PointLight") {
-				lights->GetRootComponent()->ToggleVisibility();
-
-				if (lights->GetRootComponent()->IsVisible()) {
-					//GetMesh()->PlayAnimation(_animationInteract, false);
-				} else {
-					//GetMesh()->Pause????Animation(_animationInteract, false);
-				}
+	if (!_timelineComponent->IsPlaying()) {
+		if (_bLightOn) {
+			_bLightOn = false;
+			for (auto& lights : _lightActor) {
+				lights->GetLightComponent()->SetVisibility(false);
 			}
+
+			_timelineComponent->PlayFromStart();
+			_soundComponent->Play();
+		}
+		else {
+			_bLightOn = true;
+			for (auto& lights : _lightActor) {
+				lights->GetLightComponent()->SetVisibility(true);
+			}
+
+			_timelineComponent->ReverseFromEnd();
+			_soundComponent->Play();
 		}
 	}
+}
+
+void ALightSwitch::ControlLightSwitch()
+{
+	_timelineValue = _timelineComponent->GetPlaybackPosition();
+	_curveFloatValue = _curveFloat->GetFloatValue(_timelineValue);
+
+	if (_bOldLight)
+		_lightSwitch->SetRelativeRotation(FRotator(_curveFloatValue, 0.f, 0.f));
+	else
+		_lightSwitch->SetRelativeRotation(FRotator(0.f, 0.f, _curveFloatValue));
 }
--- Source/SophiasFault/Inventory/Items/LightSwitch.h
+++ Source/SophiasFault/Inventory/Items/LightSwitch.h
@@ -5,19 +5,40 @@
 #include "LightSwitch.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class SOPHIASFAULT_API ALightSwitch : public AItem, public IInteractiveInterface
 {
 	GENERATED_BODY()
-	
+
 public:
 	ALightSwitch();
 
+	bool _bLightOn;
+
 	UPROPERTY(EditAnywhere)
-	TArray<class AActor*> _lightActor;
+	bool _bOldLight;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* _lightSwitch;
+
+	UPROPERTY(EditAnywhere)
+	TArray<class ALight*> _lightActor;
+
+	UPROPERTY(EditAnywhere, Category = "Timeline")
+	class UCurveFloat* _curveFloat;
+
+	FOnTimelineFloat _timelineCallback;
+
+	float _timelineValue;
+	float _curveFloatValue;
+
+	void BeginPlay() override;
 
 	UFUNCTION(BlueprintCallable, Category = "Interaction")
 	virtual void UseInteraction(AItem* item) override;
+
+	UFUNCTION()
+	void ControlLightSwitch();
 };
